3a5d74741a1b422b52526e0b4dcc95d523673c8d
KarloMendoza <karlomendoza.@liveramp.com>
2021-10-12 10:03:31 -0500

    add missing files

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import io.opentelemetry.api.OpenTelemetry;
 import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.SpanBuilder;
 import io.opentelemetry.api.trace.StatusCode;
 import io.opentelemetry.api.trace.Tracer;
 import io.opentelemetry.api.trace.propagation.W3CTraceContextPropagator;
@@ -9,6 +10,7 @@ import io.opentelemetry.context.Scope;
 import io.opentelemetry.context.propagation.ContextPropagators;
 import io.opentelemetry.exporter.otlp.trace.OtlpGrpcSpanExporter;
 import io.opentelemetry.sdk.OpenTelemetrySdk;
+import io.opentelemetry.sdk.autoconfigure.OpenTelemetrySdkAutoConfiguration;
 import io.opentelemetry.sdk.trace.SdkTracerProvider;
 import io.opentelemetry.sdk.trace.export.BatchSpanProcessor;
 import java.util.Date;
@@ -25,20 +27,35 @@ public class DemoApplication {
 
   public static Tracer tracer;
 
-  public static void main(String[] args) {
-
+  public static void main(String[] args) throws InterruptedException {
     SpringApplication.run(DemoApplication.class, args);
 
-    SdkTracerProvider sdkTracerProvider = SdkTracerProvider.builder()
-        .addSpanProcessor(BatchSpanProcessor.builder(OtlpGrpcSpanExporter.builder().build()).build())
-        .build();
+    System.setProperty("otel.traces.exporter", "otlp");
+    System.setProperty("otel.metrics.exporter", "none");
+    System.setProperty("otel.exporter.otlp.endpoint", "http://otel-agent.karlo:4317");
+    System.setProperty("otel.resource.attributes", "service.name=OtlpKarloTestv2");
 
-    OpenTelemetry openTelemetry = OpenTelemetrySdk.builder()
-        .setTracerProvider(sdkTracerProvider)
-        .setPropagators(ContextPropagators.create(W3CTraceContextPropagator.getInstance()))
-        .buildAndRegisterGlobal();
+    OpenTelemetry openTelemetry = OpenTelemetrySdkAutoConfiguration.initialize();
 
-    tracer = openTelemetry.getTracer("dasds");
+    tracer = openTelemetry.getTracer("karlo-test");
+
+    tracer.spanBuilder("asdas").startSpan().end();
+
+    int i = 0;
+    while (true ) {
+      i++;
+      Thread.sleep(5);
+      Span parentSpan = tracer.spanBuilder("parent").startSpan();
+      parentSpan.setAttribute("ID", i);
+      parentSpan.setAttribute("KARLO", "TEST");
+      System.out.println("current ID: " + i);
+      try (Scope scope = parentSpan.makeCurrent()) {
+        //DO actual work
+      } finally {
+        parentSpan.end();
+      }
+    }
+
   }
 
   @PostConstruct
